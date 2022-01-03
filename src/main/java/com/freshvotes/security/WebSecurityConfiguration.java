57724ddc88b20ec2322584bd1089d2d910e4d18f
murod9393 <murod93.93@bk.ru>
2022-01-03 16:37:44 -0500

    csrf.token feature added to webSecurityConfig class.

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,12 +33,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception 
 	{
-		http.csrf().disable()
-		.authorizeRequests()
+		http.authorizeRequests()
 		.antMatchers("/").permitAll()
 		.anyRequest().hasRole("USER").and()
 		.formLogin()
 		   .loginPage("/login")
+		   .defaultSuccessUrl("/dashboard")
 		    .permitAll()
 		.and()
 		.logout()
